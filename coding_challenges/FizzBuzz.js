c26e7cc411c29dba24602138c154149c820a376a
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-04-08 19:31:17 -0600

    feat: Add solution to exercise FizzBuzz

---
+++ coding_challenges/FizzBuzz.js
@@ -0,0 +1,30 @@
+// Ejercicio: FizzBuzz
+// Escribe una función que recorra los números del 1 al 100 y que:
+
+// Imprima "Fizz" si el número es múltiplo de 3.
+
+// Imprima "Buzz" si el número es múltiplo de 5.
+
+// Imprima "FizzBuzz" si el número es múltiplo de 3 y de 5.
+
+// Si no es múltiplo ni de 3 ni de 5, imprime el número.
+
+function FizzBuzz() {
+  for (let index = 1; index <= 15; index++) {
+    console.log(isMultiple(index));
+  }
+}
+
+
+function isMultiple(num) {
+  if ((num % 3) === 0 && (num % 5) === 0) {
+    return "FizzBuzz"
+  }else if ((num % 3) === 0) {
+    return "Fizz"
+  }else if ((num % 5) === 0) {
+    return "Buzz"
+  }
+  return num
+}
+
+FizzBuzz();
