e5478007e34172f097d5a06c0184420f7c0a761d
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-04-19 20:50:05 -0600

    feat: Implement functions to calculate daily rates and monthly discounted prices

---
+++ exerscism/03_Freelancer_Rates/freelancer-rates.js
@@ -0,0 +1,27 @@
+export function dayRate(ratePerHour) {
+  return ratePerHour * 8;
+}
+
+export function daysInBudget(budget, ratePerHour) {
+  return Math.round(budget / (ratePerHour * 8));
+}
+
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const ratePerDay = ratePerHour * 8;
+  const numDaysWithDiscount = Math.floor(numDays / 22) * 22;
+  const priceDiscount = Math.ceil(
+    ratePerDay * numDaysWithDiscount * (1 - discount)
+  );
+
+  const numDaysWithoutDiscount = numDays - numDaysWithDiscount;
+  const priceNormal = numDaysWithoutDiscount * ratePerDay;
+
+  return priceDiscount + priceNormal;
+}
+
+// Test cases
+console.log(dayRate(89));
+
+console.log(daysInBudget(20000, 89));
+
+console.log(priceWithMonthlyDiscount(89, 230, 0.42));
