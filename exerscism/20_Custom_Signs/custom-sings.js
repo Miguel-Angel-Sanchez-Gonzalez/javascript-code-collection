7c793829a5a38a8ba2d610cd3e0b19cc292ea26e
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-05-02 21:02:24 -0600

    feat: Add test cases for signs construction functions

--- exerscism/20_Custom_Signs/custom-sings.js
+++ exerscism/20_Custom_Signs/custom-sings.js
@@ -1,6 +1,12 @@
+// Solución al ejercicio de Exercism: "Custom Signs"
+// Enunciado tomado de Exercism.org
+
 export function buildSign(occasion, name) {
   return `Happy ${occasion} ${name}!`;
 }
+
+// Test case
+console.log(buildSign("Birthday", "Rob"));
 
 export function buildBirthdaySign(age) {
   return `Happy Birthday! What a ${
@@ -8,14 +14,22 @@ export function buildBirthdaySign(age) {
   } fellow you are.`;
 }
 
+// Test cases
+console.log(buildBirthdaySign(50));
+console.log(buildBirthdaySign(45));
+
 export function graduationFor(name, year) {
   return `Congratulations ${name}!
   Class of ${year}`;
 }
+
+// Test case
+console.log(graduationFor("Hannah", 2022));
 
 export function costOf(sign, currency) {
   const priceSign = sign.length * 2 + 20;
   return `Your sign costs ${priceSign.toFixed(2)} ${currency}.`;
 }
 
+// Test case
 console.log(costOf("Happy Birthday Rob!", "dollars"));
