8d637a1fbb73fc05d7c52e0e7d3ae821f7b638e6
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-05-03 19:51:00 -0600

    feat: Implement Size, Position and ProgramWindow classes with test cases

---
+++ exerscism/21_Windowing_System/windowing-system.js
@@ -0,0 +1,76 @@
+//--Class Size
+function Size(width = 80, height = 60) {
+  this.width = width;
+  this.height = height;
+}
+
+Size.prototype.resize = function (width, height) {
+  this.width = width;
+  this.height = height;
+};
+
+// Test cases (Class Size)
+const size = new Size(1080, 764);
+console.log(size.width);
+console.log(size.height);
+
+size.resize(1920, 1080);
+console.log(size.width);
+console.log(size.height);
+
+const size2 = new Size();
+console.log(size2.width);
+console.log(size2.height);
+
+//--Class Position
+function Position(x = 0, y = 0) {
+  this.x = x;
+  this.y = y;
+}
+
+Position.prototype.move = function (newX, newY) {
+  this.x = newX;
+  this.y = newY;
+};
+
+// Test cases (Class Position)
+const point = new Position();
+console.log(point.x);
+console.log(point.y);
+
+point.move(100, 200);
+console.log(point.x);
+console.log(point.y);
+
+const p2 = new Position(10, 20);
+console.log(p2.x);
+console.log(p2.y);
+
+// -- Class ProgramWindow
+class ProgramWindow {
+  constructor() {
+    this.screenSize = new Size(800, 600);
+    this.size = new Size();
+    this.position = new Position();
+  }
+
+  resize(newSize) {
+    const maxWidth = this.screenSize.width - this.position.x;
+    const maxHeight = this.screenSize.height - this.position.y;
+
+    const newWidth = Math.max(1, Math.min(newSize.width, maxWidth));
+    const newHeight = Math.max(1, Math.min(newSize.height, maxHeight));
+
+    this.size = new Size(newWidth, newHeight);
+  }
+}
+
+// Test cases (Class ProgramWindow)
+const programWindow = new ProgramWindow();
+console.log(programWindow.screenSize.width);
+console.log(programWindow.screenSize.height);
+
+const newSize = new Size(600, 400);
+programWindow.resize(newSize);
+console.log(programWindow.size.width);
+console.log(programWindow.size.height);
