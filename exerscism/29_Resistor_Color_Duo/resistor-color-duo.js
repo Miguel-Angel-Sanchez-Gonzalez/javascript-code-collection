56ab7008db6bf79ae9c7ea847ed27e09589b3827
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-05-11 21:05:13 -0600

    refactor: Correct calculation in the decodedValue function to return the correct value of the resistor

--- exerscism/29_Resistor_Color_Duo/resistor-color-duo.js
+++ exerscism/29_Resistor_Color_Duo/resistor-color-duo.js
@@ -1,7 +1,10 @@
+// Solución al ejercicio de Exercism: "Resistor Color Duo"
+// Enunciado tomado de Exercism.org
+
 export const decodedValue = (color) => {
   const color1 = COLORS.indexOf(color[0]);
   const color2 = COLORS.indexOf(color[1]);
-  return color1 * 10 + color2;
+  return parseInt("" + color1 + "" + color2);
 };
 
 export const COLORS = [
