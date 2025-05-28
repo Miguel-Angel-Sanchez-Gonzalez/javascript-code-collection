aa240af775bb7064eddaf4a5c1b9e7dcffb76a10
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-05-27 20:32:25 -0600

    refactor: Optimize sumOfSquares and squareOfSum methods using mathematical formulas

--- exerscism/43_Difference_of_Squares/difference-of-squares.js
+++ exerscism/43_Difference_of_Squares/difference-of-squares.js
@@ -6,24 +6,18 @@ export class Squares {
     if (typeof number !== "number" || number < 0) {
       throw new Error("Input must be a non-negative integer");
     }
+    a;
     this.number = number;
   }
 
   get sumOfSquares() {
-    let result = 0;
-    for (let index = 1; index <= this.number; index++) {
-      result += index * index;
-    }
-    return result;
+    const n = this.number;
+    return (n * (n + 1) * (2 * n + 1)) / 6; // Fórmula: suma de los primeros n cuadrados: n(n+1)(2n+1)/6
   }
 
   get squareOfSum() {
-    let result = 0;
-    for (let index = 1; index <= this.number; index++) {
-      result += index;
-    }
-
-    return result * result;
+    const sum = (this.number * (this.number + 1)) / 2; // Fórmula: suma de los primeros n números: n(n+1)/2
+    return sum * sum;
   }
 
   get difference() {
