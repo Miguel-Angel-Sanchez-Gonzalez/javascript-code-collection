4bf50d3c396d75d1045c821ce47441fa88de4844
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-06-15 20:07:50 -0600

    refactor: Simplify the transpose function for improved readability

--- exerscism/53_Transpose/transpose.js
+++ exerscism/53_Transpose/transpose.js
@@ -1,34 +1,35 @@
 // Solución al ejercicio de Exercism: "Transpose"
 // Enunciado tomado de Exercism.org
 
-export const transpose = (array) => {
-  if (array.length === 0) return [];
-
-  const maxLength = Math.max(...array.map((line) => line.length));
-  const result = [];
-
-  for (let i = 0; i < maxLength; i++) {
-    let row = "";
-    for (let j = 0; j < array.length; j++) {
-      row += array[j][i] || " ";
+export const transpose = (input) => {
+  let result = [];
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      let str = input[i][j];
+      let rowLength = 0;
+      if (result[j]) {
+        rowLength = result[j].length;
+      }
+      if (i > rowLength) {
+        str = str.padStart(i - rowLength + 1, " ");
+      }
+      result[j] ? (result[j] += str) : (result[j] = str);
     }
-    result.push(row);
   }
-
   return result;
 };
 
 // Test cases
-console.log(transpose([]));
-console.log(transpose(["A1"]));
-console.log(transpose(["A", "1"]));
-console.log(transpose(["ABC", "123"]));
-console.log(transpose(["Single line."]));
+// console.log(transpose([]));
+// console.log(transpose(["A1"]));
+// console.log(transpose(["A", "1"]));
+// console.log(transpose(["ABC", "123"]));
+// console.log(transpose(["Single line."]));
 console.log(transpose(["The fourth line.", "The fifth line."]));
-console.log(transpose(["The first line.", "The second line."]));
-console.log(
-  transpose(["The longest line.", "A long line.", "A longer line.", "A line."])
-);
-console.log(transpose(["HEART", "EMBER", "ABUSE", "RESIN", "TREND"]));
-console.log(transpose(["FRACTURE", "OUTLINED", "BLOOMING", "SEPTETTE"]));
-console.log(transpose(["T", "EE", "AAA", "SSSS", "EEEEE", "RRRRRR"]));
+// console.log(transpose(["The first line.", "The second line."]));
+// console.log(
+//   transpose(["The longest line.", "A long line.", "A longer line.", "A line."])
+// );
+// console.log(transpose(["HEART", "EMBER", "ABUSE", "RESIN", "TREND"]));
+// console.log(transpose(["FRACTURE", "OUTLINED", "BLOOMING", "SEPTETTE"]));
+// console.log(transpose(["T", "EE", "AAA", "SSSS", "EEEEE", "RRRRRR"]));
