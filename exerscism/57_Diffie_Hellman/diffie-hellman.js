5aca228ddec0ac52579341b144dc353ba94d63fb
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-06-24 19:23:29 -0600

    refactor: Optimize the isPrimo function for prime validation

--- exerscism/57_Diffie_Hellman/diffie-hellman.js
+++ exerscism/57_Diffie_Hellman/diffie-hellman.js
@@ -3,7 +3,7 @@
 
 export class DiffieHellman {
   constructor(p, g) {
-    if (!esPrimo(p) || !esPrimo(g))
+    if (!isPrimo(p) || !isPrimo(g))
       throw new Error("The constructor arguments are not prime");
 
     this.p = p;
@@ -17,13 +17,18 @@ export class DiffieHellman {
   getPrivateKey() {}
 }
 
-export function esPrimo(n) {
+export function isPrimo(n) {
   if (n <= 1) return false;
-  for (let i = 2; i < n; i++) {
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) return false;
   }
   return true;
 }
+
+isPrimo(7919);
 
 // Test cases
 new DiffieHellman(10, 13);
