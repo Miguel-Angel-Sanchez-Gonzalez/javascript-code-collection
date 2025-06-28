f495b7bce64929bfb10d42de94f3a62a8baf7a83
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-06-27 23:25:14 -0600

    feat: Implement create appointment function for Exercism exercise

---
+++ exerscism/60_Appointment_Time/appointment-time.js
@@ -0,0 +1,57 @@
+// Solución al ejercicio de Exercism: "Appointment-time"
+// Enunciado tomado de Exercism.org
+
+export const createAppointment = (days, now = Date.now()) =>
+  new Date((now instanceof Date ? now.getTime() : now) + 86_400_000 * days);
+
+/**
+ * Generate the appointment timestamp
+ * @param {Date} appointmentDate
+ * @returns {string} timestamp
+ */
+export function getAppointmentTimestamp(appointmentDate) {
+  throw new Error("Remove this line and implement the function");
+}
+
+/**
+ * Get details of an appointment
+ * @param {string} timestamp (ISO 8601)
+ * @returns {Record<'year' | 'month' | 'date' | 'hour' | 'minute', number>} the appointment details
+ */
+export function getAppointmentDetails(timestamp) {
+  throw new Error("Remove this line and implement the function");
+}
+
+/**
+ * Update an appointment with given options
+ * @param {string} timestamp (ISO 8601)
+ * @param {Partial<Record<'year' | 'month' | 'date' | 'hour' | 'minute', number>>} options
+ * @returns {Record<'year' | 'month' | 'date' | 'hour' | 'minute', number>} the appointment details
+ */
+export function updateAppointment(timestamp, options) {
+  throw new Error("Remove this line and implement the function");
+}
+
+/**
+ * Get available time in seconds (rounded) between two appointments
+ * @param {string} timestampA (ISO 8601)
+ * @param {string} timestampB (ISO 8601)
+ * @returns {number} amount of seconds (rounded)
+ */
+export function timeBetween(timestampA, timestampB) {
+  throw new Error("Remove this line and implement the function");
+}
+
+/**
+ * Get available times between two appointment
+ * @param {string} appointmentTimestamp (ISO 8601)
+ * @param {string} currentTimestamp (ISO 8601)
+ */
+export function isValid(appointmentTimestamp, currentTimestamp) {
+  throw new Error("Remove this line and implement the function");
+}
+
+// Test cases
+const startDate = new Date("2022-10-05T23:28:43+06:00");
+const result = createAppointment(4, startDate);
+console.log(result);
