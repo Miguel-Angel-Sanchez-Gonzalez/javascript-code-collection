6055aeb51b229226ddaa8a09c990f81753123617
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-06-30 22:04:37 -0600

    feat: Implement isValid function to check appointment validity

--- exerscism/60_Appointment_Time/appointment-time.js
+++ exerscism/60_Appointment_Time/appointment-time.js
@@ -53,26 +53,24 @@ export function timeBetween(timestampA, timestampB) {
   );
 }
 
-/**
- * Get available times between two appointment
- * @param {string} appointmentTimestamp (ISO 8601)
- * @param {string} currentTimestamp (ISO 8601)
- */
 export function isValid(appointmentTimestamp, currentTimestamp) {
-  throw new Error("Remove this line and implement the function");
+  return new Date(appointmentTimestamp) - new Date(currentTimestamp) > 0
+    ? true
+    : false;
 }
 
 // Test cases
-// const startDate = new Date("2022-10-05T23:28:43+06:00");
-// const result = createAppointment(4, startDate);
-// console.log(result);
+const startDate = new Date("2022-10-05T23:28:43+06:00");
+const result = createAppointment(4, startDate);
+console.log(result);
 
-// const appointment = new Date(Date.UTC(2010, 6, 16, 12, 42, 0, 0));
-// const result2 = getAppointmentTimestamp(appointment);
-// console.log(result2);
+const appointment = new Date(Date.UTC(2010, 6, 16, 12, 42, 0, 0));
+const result2 = getAppointmentTimestamp(appointment);
+console.log(result2);
 
-// console.log(getAppointmentDetails("2022-02-09T09:20:00.000"));
+console.log(getAppointmentDetails("2022-02-09T09:20:00.000"));
 
-// console.log(updateAppointment("2022-02-09T09:20:00.000", { month: 6 }));
+console.log(updateAppointment("2022-02-09T09:20:00.000", { month: 6 }));
 
 console.log(timeBetween("2022-12-12T09:20:00.000", "2022-12-18T08:30:00.000"));
+console.log(isValid("2022-02-11T23:00:00.000", "2022-02-08T23:00:00.000"));
