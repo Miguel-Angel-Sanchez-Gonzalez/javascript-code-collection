528a73a2dcb4c60db80eb60891820dd1bce7a55f
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-08-05 21:55:15 -0600

    feat: Refactor List constructor and implement concat method

--- exerscism/67_List_Ops/list-ops.js
+++ exerscism/67_List_Ops/list-ops.js
@@ -5,7 +5,16 @@
 
 export class List {
   constructor(array = []) {
-    this.list = array;
+    this.list = [];
+
+    for (let item of array) {
+      if (item instanceof List) {
+        // Solo agregamos si tiene valores
+        this.list.push(...item.list.filter((val) => val !== undefined));
+      } else if (item !== undefined) {
+        this.list.push(item);
+      }
+    }
   }
 
   append(secondList) {
@@ -16,8 +25,8 @@ export class List {
     return this.list;
   }
 
-  concat() {
-    throw new Error("Remove this line and implement the function");
+  concat(newList) {
+    return new List([...this.values, ...newList.list]);
   }
 
   filter() {
@@ -45,7 +54,17 @@ export class List {
   }
 }
 
+// Test case append
 const list1 = new List([1, 2]);
 const list2 = new List([2, 3, 4, 5]);
 
-console.log(list1.append(list2));
+// console.log(list1.append(list2));
+
+// Test case concat
+const list3 = new List([1, 2]);
+const list4 = new List([3]);
+const list5 = new List([]);
+const list6 = new List([4, 5, 6]);
+const listOfLists = new List([list4, list5, list6]);
+
+console.log(list3.concat(listOfLists).values);
