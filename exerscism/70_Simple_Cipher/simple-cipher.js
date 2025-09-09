92fd0b5b936a31db03cba09a7c71a55efeee46fc
Miguel Angel Sanchez Gonzalez <miguelxangel1492@gmail.com>
2025-09-08 22:42:13 -0600

    fix: Correct typographic errors in the definition of the alphabet and improve the encode function

--- exerscism/70_Simple_Cipher/simple-cipher.js
+++ exerscism/70_Simple_Cipher/simple-cipher.js
@@ -1,7 +1,7 @@
 // Solución al ejercicio de Exercism: "Simple Cipher"
 // Enunciado tomado de Exercism.org
 
-const alfabeth = {
+const alphabet = {
   0: "a",
   1: "b",
   2: "c",
@@ -30,16 +30,23 @@ const alfabeth = {
   25: "z",
 };
 
+function findKeyByValue(obj, findValue) {
+  for (let key in obj) {
+    if (obj[key] === findValue) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
 function randomKey(lengthKey) {
   let newKey = "";
   const min = 0;
-  const max = Object.keys(alfabeth).length - 1;
-
-  console.log(max);
+  const max = Object.keys(alphabet).length - 1;
 
   while (newKey.length < lengthKey) {
     const random = Math.floor(Math.random() * (max - min + 1)) + min;
-    const letter = alfabeth[random];
+    const letter = alphabet[random];
     newKey += letter;
   }
 
@@ -51,8 +58,41 @@ export class Cipher {
     this._key = key;
   }
 
-  encode() {
-    throw new Error("Remove this line and implement the function");
+  encode(plaintext) {
+    if (plaintext.length > this._key.length) {
+      const keyArr = this._key.split("");
+      let count = 0;
+      while (plaintext.length > this._key.length) {
+        if (keyArr[count] !== undefined) {
+          this._key += keyArr[count];
+          count++;
+        } else {
+          count = 0;
+        }
+      }
+    }
+
+    let arrEncodedText = [];
+    const alphabetLength = Object.values(alphabet).length;
+
+    for (let i = 0; i < plaintext.length; i++) {
+      let tempKeyTextPlane = Number(findKeyByValue(alphabet, plaintext[i]));
+      let tempKey = Number(findKeyByValue(alphabet, this._key[i]));
+      let sumKeys = tempKeyTextPlane + tempKey;
+
+      if (sumKeys > alphabetLength) {
+        sumKeys = sumKeys % alphabetLength;
+      }
+
+      arrEncodedText.push(sumKeys);
+    }
+
+    let result = "";
+
+    arrEncodedText.forEach((element) => {
+      result += alphabet[element];
+    });
+    return result;
   }
 
   decode() {
@@ -65,5 +105,14 @@ export class Cipher {
 }
 
 // Test cases
-const cipher = new Cipher();
-console.log(cipher.key);
+const cipher = new Cipher("abcd");
+console.log(cipher.encode("hello"));
+
+const cipher2 = new Cipher("d");
+console.log(cipher2.encode("iamapandabear"));
+
+const cipher3 = new Cipher("lemon");
+console.log(cipher3.encode("attackatdawn"));
+
+const cipher4 = new Cipher("tomato");
+console.log(cipher4.encode("y"));
